105df28154db0e56a336960f2e57e65e31febcec
Ari Carr <acarr@brandeis.edu>
2019-03-20 18:01:38 -0400

    Updated for schema denormalization

--- app.rb
+++ app.rb
@@ -8,20 +8,22 @@ unless Sinatra::Base.production?
 end
 
 post '/new_tweet/:id' do
-  id = params[:id]
-  author = Tweet.find(id).author
-  import_timeline_pieces(author.follows_to_me.map { |f| [f.follower_id, id] })
+  t = Tweet.find(params[:id])
+  author = t.author
+  mapped = author.follows_to_me.map { |f| [f.follower_id, t.id, t.body, t.created_on, t.author_handle] }
+  import_timeline_pieces(mapped)
   status 200
 end
 
 post '/new_follower/:followee_id/:follower_id' do
   followee_tweets = Tweet.where(author_id: params[:followee_id])
   follower_id = params[:follower_id]
-  import_timeline_pieces(followee_tweets.map { |t| [follower_id, t.id] })
+  mapped = followee_tweets.map { |t| [follower_id, t.id, t.body, t.created_on, t.author_handle] }
+  import_timeline_pieces(mapped)
   status 200
 end
 
 def import_timeline_pieces(pieces)
-  columns = %i[timeline_owner_id tweet_id]
+  columns = %i[timeline_owner_id tweet_id tweet_body tweet_created_on tweet_author_handle]
   TimelinePiece.import columns, pieces
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,24 +20,33 @@ describe 'NanoTwitter' do
     names = %w[ari brad yang pito]
     users = names.map { |s| User.create(name: s.capitalize, handle: "@#{s}", password: "#{s}123") }
     @ari, @brad, @yang, @pito = users
+    @tweet = Tweet.new(author: @ari, body: 'I <3 Scalability', created_on: Time.now, author_handle: @ari.handle)
   end
 
   it 'can fanout tweets' do
     [@brad, @yang, @pito].each { |u| u.followees << @ari }
-    tweet = Tweet.create(author: @ari, body: 'I <3 Scalability')
-    post "/new_tweet/#{tweet.id}"
+    @tweet.save
+    post "/new_tweet/#{@tweet.id}"
     [@brad, @yang, @pito].each do |u|
-      u.timeline_tweets.count.must_equal 1
-      u.timeline_tweets.first.must_equal tweet
+      u.timeline_pieces.count.must_equal 1
+      t = u.timeline_pieces.first
+      t.tweet.must_equal @tweet
+      t.tweet_body.must_equal @tweet.body
+      t.tweet_created_on.must_equal @tweet.created_on
+      t.tweet_author_handle.must_equal @tweet.author.handle
     end
   end
 
   it 'can distribute tweets to new followers' do
-    tweet = Tweet.create(author: @ari, body: 'I <3 Scalability')
+    @tweet.save
     [@brad, @yang, @pito].each do |u|
       post "/new_follower/#{@ari.id}/#{u.id}"
-      u.timeline_tweets.count.must_equal 1
-      u.timeline_tweets.first.must_equal tweet
+      u.timeline_pieces.count.must_equal 1
+      t = u.timeline_pieces.first
+      t.tweet.must_equal @tweet
+      t.tweet_body.must_equal @tweet.body
+      t.tweet_created_on.must_equal @tweet.created_on
+      t.tweet_author_handle.must_equal @tweet.author.handle
     end
   end
 end
